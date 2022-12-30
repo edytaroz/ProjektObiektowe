331a340183ea7e1f131d8adbb2e60d527167ac4d
edytaroz <edytaroz11@poczta.fm>
2022-12-30 12:38:18 +0100

    Animal Variants Done

--- src/main/java/org/agh/ics/oop/AnimalKS.java
+++ src/main/java/org/agh/ics/oop/AnimalKS.java
@@ -1,4 +1,8 @@
 package org.agh.ics.oop;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //wariant Korekta i Szalony
 public class AnimalKS extends AnimalSzalony{
     public AnimalKS(int energyLoss, int energy, int childEnergy, int lenOfGenome, int plantEnergy, int satietyLevel, Vector2d position) {
@@ -10,5 +14,21 @@ public class AnimalKS extends AnimalSzalony{
         super(parent1, parent2);
         GenomeKorekta genome = new GenomeKorekta(parent1, parent2);
         this.genes = genome.getGenes();
+    }
+    @Override
+    public List<Animal> conflictReproduction(List<Animal> animals){//assumes there are at least 2 animals
+        List<Animal> possibleParents = new ArrayList<>();
+        for (int i = 0; i < animals.size(); i++){
+            if (animals.get(i).energy >= animals.get(i).satietyLevel){
+                possibleParents.add(animals.get(i));
+            }
+        }
+        possibleParents.sort(new Animal.ReproductionComparator());
+        List<Animal> children = new ArrayList<>();
+        for (int i = 0; i < possibleParents.size()/2; i++){
+            Animal child = new AnimalKS(possibleParents.get(2*i),possibleParents.get(2*i+1));
+            children.add(child);
+        }
+        return children;
     }
 }
--- src/main/java/org/agh/ics/oop/AnimalLS.java
+++ src/main/java/org/agh/ics/oop/AnimalLS.java
@@ -1,4 +1,8 @@
 package org.agh.ics.oop;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //wariant Losowy i Szalony
 public class AnimalLS extends AnimalSzalony {
 
@@ -11,5 +15,21 @@ public class AnimalLS extends AnimalSzalony {
         super(parent1, parent2);
         GenomeLosowe genome = new GenomeLosowe(parent1, parent2);
         this.genes = genome.getGenes();
+    }
+    @Override
+    public List<Animal> conflictReproduction(List<Animal> animals){//assumes there are at least 2 animals
+        List<Animal> possibleParents = new ArrayList<>();
+        for (int i = 0; i < animals.size(); i++){
+            if (animals.get(i).energy >= animals.get(i).satietyLevel){
+                possibleParents.add(animals.get(i));
+            }
+        }
+        possibleParents.sort(new Animal.ReproductionComparator());
+        List<Animal> children = new ArrayList<>();
+        for (int i = 0; i < possibleParents.size()/2; i++){
+            Animal child = new AnimalLS(possibleParents.get(2*i),possibleParents.get(2*i+1));
+            children.add(child);
+        }
+        return children;
     }
 }
