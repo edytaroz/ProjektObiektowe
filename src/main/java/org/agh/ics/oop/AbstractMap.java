3038744f10ab8aa4bdf3a6a26f175e29a6956951
naumiec <92885584+naumiec@users.noreply.github.com>
2023-01-02 19:23:09 +0100

    AbstractMap

--- src/main/java/org/agh/ics/oop/AbstractMap.java
+++ src/main/java/org/agh/ics/oop/AbstractMap.java
@@ -2,7 +2,10 @@ package org.agh.ics.oop;
 
 import java.util.ArrayList;
 
-public class AbstractMap implements IPositionChangeObserver {
+public abstract class AbstractMap implements IPositionChangeObserver {
+    public int width;
+    public int height;
+
     public ArrayList<Animal> animals = new ArrayList<>();
     public ArrayList<Vector2d> deadAnimals = new ArrayList<>();
     public ArrayList<Grass> grasses = new ArrayList<>();
