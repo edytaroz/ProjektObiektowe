7cbbfd7e26a7ac90c58cb128247440319da32bb0
naumiec <92885584+naumiec@users.noreply.github.com>
2023-01-02 19:12:17 +0100

    Grass

---
+++ src/main/java/org/agh/ics/oop/Grass.java
@@ -0,0 +1,12 @@
+package org.agh.ics.oop;
+
+public class Grass extends AbstractMapElement {
+    public Grass(Vector2d position) {
+        super(position);
+    }
+
+    @Override
+    public String getImagePath() {
+        return null;
+    }
+}
