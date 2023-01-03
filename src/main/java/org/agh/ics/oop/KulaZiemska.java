fd23bce42067d911739658e8ffe5cb2471dce7d7
naumiec <92885584+naumiec@users.noreply.github.com>
2023-01-03 09:10:24 +0100

    quick fix

--- src/main/java/org/agh/ics/oop/KulaZiemska.java
+++ src/main/java/org/agh/ics/oop/KulaZiemska.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 
-public class Portal extends AbstractMap {
-    public Portal(int width, int height) {
+public class KulaZiemska extends AbstractMap {
+    public KulaZiemska(int width, int height) {
         this.lowerLeft = new Vector2d(0, 0);
         this.upperRight = new Vector2d(width, height);
     }
